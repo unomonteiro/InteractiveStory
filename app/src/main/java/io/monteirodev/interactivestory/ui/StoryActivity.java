870e43b5d6a872191e2e8f122844e5521282e83b
Carlos Monteiro <unomonteiro@gmail.com>
2016-04-15 16:03:13 +0100

    Story and activities.

--- app/src/main/java/io/monteirodev/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/io/monteirodev/interactivestory/ui/StoryActivity.java
@@ -1,13 +1,29 @@
 package io.monteirodev.interactivestory.ui;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import io.monteirodev.interactivestory.R;
+import io.monteirodev.interactivestory.model.Page;
+import io.monteirodev.interactivestory.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
+    private Page mCurrentPage;
+
 
     public static final String TAG = StoryActivity.class.getSimpleName();
     @Override
@@ -16,12 +32,61 @@ public class StoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra("name");
-        if (name == null){
-            name = "Friend";
+        mName = intent.getStringExtra("name");
+        if (mName == null){
+            mName = "Friend";
         }
 
-        Log.d(TAG, name);
+        Log.d(TAG, mName);
+
+        mImageView = (ImageView)findViewById(R.id.storyImageView);
+        mTextView = (TextView)findViewById(R.id.storyTextView);
+        mChoice1 = (Button)findViewById(R.id.choiceButton1);
+        mChoice2 = (Button)findViewById(R.id.choiceButton2);
+
+        loadPage(0);
+    }
+
+    private void loadPage(int choice){
+        mCurrentPage = mStory.getPage(choice);
+        //Drawable drawable = getResources().getDrawable(page.getImageId());
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(), mCurrentPage.getImageId(), null);
+        mImageView.setImageDrawable(drawable);
+
+        String pageText = mCurrentPage.getText();
+        // Add the name if placeholder is found.
+        pageText = String.format(pageText,mName);
+        mTextView.setText(pageText);
+if (mCurrentPage.isFinal()){
+    mChoice1.setVisibility(View.INVISIBLE);
+    mChoice2.setText("PLAY AGAIN");
+    mChoice2.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    });
+} else {
+    mChoice1.setText(mCurrentPage.getChoice1().getText());
+    mChoice2.setText(mCurrentPage.getChoice2().getText());
+
+    mChoice1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int nextPage = mCurrentPage.getChoice1().getNextPage();
+            loadPage(nextPage);
+        }
+    });
+
+
+    mChoice2.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int nextPage = mCurrentPage.getChoice2().getNextPage();
+            loadPage(nextPage);
+        }
+    });
+}
 
     }
 }
